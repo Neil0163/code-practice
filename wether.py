262f471e64cefd570af5011241f169d7ba4d09be
Neil Uddin <neil.uddin@hotmail.com>
2023-11-08 16:10:22 +0000

    First commit of wether file

---
+++ wether.py
@@ -0,0 +1,16 @@
+def as_sun_lover(temp):
+    if (temp) > 25:
+        return 'Great'
+    else:
+        return 'Not great'
+    
+def as_snow_lover(temp):
+    if (temp) < 0:
+        return "Great"
+    else: 
+        return "Not great" 
+
+def report_wether(temp, preference_func):
+    return preference_func(temp)
+
+print(report_wether(26, as_snow_lover))
